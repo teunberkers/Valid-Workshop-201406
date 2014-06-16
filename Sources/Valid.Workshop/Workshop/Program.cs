7473b7ae309bf383a6c9ac96befe6c51e54d8c9a
Teun Berkers <teun_berkers@hotmail.com>
2014-06-16 22:20:30 +0200

    Initial version of workshop project

---
+++ Sources/Valid.Workshop/Workshop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Workshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = DoSomeWork();
+            Console.WriteLine("Result={0}", result);
+
+            Console.ReadKey();
+
+        }
+
+        static int DoSomeWork()
+        {
+            Func<int, int> fib = null; 
+            fib = x => x > 1 ? fib(x - 1) + fib(x - 2) : x;
+
+            return fib(25);
+        }
+    }
+}
